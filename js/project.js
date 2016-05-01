2609b303429af598625f88abc157a35ceb0b2f01
Katrina P <katrina.prather82@gmail.com>
2016-05-01 18:34:41 -0400

    JS Update
    Add google analytics and clean up code

--- js/project.js
+++ js/project.js
@@ -1,27 +1,43 @@
-	
-//works - navigation ease to section on page
-	function goToByScroll(id){
-		$('html, body').animate({scrollTop: $("#"+id).offset().top},2000);
-		id.preventDefault();
 
-	}
+//Keeps year in footer up to date automatically
+var today = new Date();
+var year = today.getFullYear();	
+
+var el = document.getElementById('footer-copyright');
+el.innerHTML = '<p>&copy;' + year + ' Katrina Prather</p>';
 
 
+
+
+//Navigation ease to section on page
+function goToByScroll(id){
+	$('html, body').animate({scrollTop: $("#"+id).offset().top},2000);
+	id.preventDefault();
+}
+
+//Double chevron that will scroll to top of page after clicked
 function backToTop(){
 		$('.scrollup').fadeToggle();
 		$('html, body').animate({
 			scrollTop: 0},600);
 		return false;
-		
-
 }
+
+//Google Analytics
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-77179345-1', 'auto');
+  ga('send', 'pageview');
+
 
 
 $(function(){
 
-
 	//Variable holds the position of the bottom of the home page
-	//to determine when to apply nav changes -was working
+	//to determine when to apply nav changes 
 	var heroBottom = $("#hero").offset().top + $("#hero").height();
 	
 
@@ -38,13 +54,11 @@ $(function(){
 			$('.scrollup').hide();
 		}
 
+	});
 
-	});//works 
 
-
-	
 	// Clicking the arrow at the bottom of the hero page will take you
-	//to the top of my tagline -was working
+	//to the top of my tagline 
 	$('.icon').click(function(){
 
 		$('html, body').animate({
@@ -61,34 +75,5 @@ $(function(){
     	$(this).parent().toggleClass('slideDown');
   	});
 
-/*for double arrows to navigate to top of page -need this to work
-	$(window).scroll(function(){
-		if ($(this).scrollTop()>100){
-			$('.scrollup').fadeIn();
-		}
-		else{
-			$('.scrollup').fadeOut();
-		}
-	});
 
-	$('.scrollup').click(function(){
-
-		$('html, body').animate({
-			scrollTop: 0},600);
-		return false;
-		});
-
-
-	});
-*/
-/*
-	$('#active-about').click(function(){
-
-		$('html, body').animate({
-			scrollTop: $('#tagline').offset().top}, 1000);
-			
-
-	});
-
-*/
 });
